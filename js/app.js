2632a19a645cf4a888e323e3cf56ce3d362b3537
Riley Andert <Randert81@gmail.com>
2015-12-06 17:28:02 -0800

    Added login/signup page

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('SnowcastApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
+angular.module('SnowcastApp', ['ngSanitize', 'ui.router', 'ui.bootstrap','firebase'])
 .config(function($stateProvider){
 	$stateProvider
 		.state('home', {
@@ -28,6 +28,11 @@ angular.module('SnowcastApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
 			templateUrl: 'partials/login.html',
 			controller: 'LoginCtrl'
 		})
+		.state('signup', {
+			url: '/signup',
+			templateUrl: 'partials/signup.html',
+			controller: 'LoginCtrl'
+		})
 })
 
 .controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
@@ -51,6 +56,82 @@ angular.module('SnowcastApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
 
 }])
 
-.controller('LoginCtrl', ['$scope', '$http', function($scope, $http) {
+.controller('LoginCtrl', ['$scope', '$http', '$firebaseAuth', '$firebaseArray', '$firebaseObject', function($scope, $http, $firebaseAuth,$firebaseArray, $firebaseObject ) {
+		var baseRef = new Firebase('https://snowcast343d.firebaseio.com/');
+		var usersRef = baseRef.child('users');
 
-}])
+		$scope.users = $firebaseObject(usersRef);
+
+		$scope.newUser = {}; //for sign-in
+
+		/* Authentication */
+		var Auth = $firebaseAuth(baseRef);
+
+		$scope.signUp = function() {
+			// Create user
+			Auth.$createUser({
+				'email': $scope.newUser.email,
+				'password': $scope.newUser.password,
+			})
+
+				// Once the user is created, call the logIn function
+				.then($scope.signIn)
+
+				// Once logged in, set and save the user data
+				.then(function(authData) {
+					console.log("logged in");
+					$scope.userId = authData.uid; //save userId
+
+					$scope.users[authData.uid] = { //set up new information in our users object
+						firstName:$scope.newUser.firstName,
+						lastName:$scope.newUser.lastName,
+					}
+					$scope.users.$save(); //save to firebase
+				})
+
+				// Catch any errors
+				.catch(function(error) {
+					console.error("Error: ", error);
+				});
+		};
+
+		// LogIn function
+		$scope.signIn = function() {
+			var ref = new Firebase('https://snowcast343d.firebaseio.com/');
+			console.log('log in');
+			ref.authWithPassword({
+				email: $scope.newUser.email,
+				password: $scope.newUser.password
+			}, function(error, authData) {
+				if (error) {
+					console.log("Login Failed!", error);
+				} else {
+					console.log("Authenticated successfully with payload:", authData);
+				}
+			});
+
+		};
+
+		// LogOut function
+		$scope.logOut = function() {
+			Auth.$unauth();
+		};
+
+		// any time auth status updates, set the userId so we know
+		Auth.$onAuth(function(authData) {
+			if(authData) {
+				$scope.userId = authData.uid;
+			}
+			else {
+				$scope.userId = undefined;
+			}
+		});
+
+		// Test if already logged in (first time)
+		var authData = Auth.$getAuth();
+		if (authData) {
+			$scope.userId = authData.uid;
+		}
+
+	}]);
+
