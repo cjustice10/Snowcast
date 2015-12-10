b69f90e2358932511e75273371cab506b9ddb1ec
Patricia Au <patau@uw.edu>
2015-12-09 20:13:51 -0800

    Adds .directory to snowviews partial so page doesn't go to 404 page

--- js/app.js
+++ js/app.js
@@ -54,21 +54,37 @@ angular.module('SnowcastApp', ['ngSanitize', 'ui.router', 'ui.bootstrap', 'fireb
 
 .controller('SnowviewsCtrl', ['$scope', '$http', '$firebaseArray', '$firebaseObject', 'firebaseService', function($scope, $http, $firebaseArray, $firebaseObject, firebaseService) {
 
-	$scope.skiResortNames = ['Crystal Mountain', 'Mt. Baker', 'Stevens Pass', 'Summit Central at Snoqualmie'];
+	$scope.ratingDrops = ['1', '2', '3', '4', '5'];
+	$scope.ratingDrop = '';
+	$scope.skiResortNames = ['Crystal Mountain', 'Mt. Baker', 'Stevens Pass'];
 	$scope.skiResortName = '';
-	// firebaseService.skiResortName = function(skiResortName) {
-	// 	service.resortName.push(info);
-	// }
+
 
 	$scope.submitFunction = function() {
+
 		//process the form get the data
+		var storedRating = $scope.ratingDrop;
 		var storedResortName = $scope.skiResortName;
 		var storedResortDesc = $scope.skiResortDesc;
 		//Get name of user, store it
-		firebaseService.storeReview(storedResortName, storedResortDesc);
+		firebaseService.storeReview(storedRating, storedResortName, storedResortDesc);
+
 	}
 
+	//$scope.rating = 3;
+
 }])
+
+.directive('eatClick', function() {
+    return function(scope, element, attrs) {
+        //angular.element('#submitButton').bind('click', function(event) {
+        $('#submitButton').click(function(event) {
+            event.preventDefault();
+        });
+    }
+})
+
+
 
 .controller('LoginCtrl', ['$scope', '$http', '$firebaseAuth', '$firebaseArray', '$firebaseObject', function($scope, $http, $firebaseAuth,$firebaseArray, $firebaseObject ) {
 
@@ -78,29 +94,33 @@ angular.module('SnowcastApp', ['ngSanitize', 'ui.router', 'ui.bootstrap', 'fireb
 
 // WORKS STARTING HERE
 .factory('firebaseService', function($firebaseArray) {
+	var ref = new Firebase("https://snowcast343d.firebaseio.com");
+
 	var service = {};
 
 	service.userInfo = {};
 
-	var ref = new Firebase("https://snowcast343d.firebaseio.com");
-	var reviewsRef = ref.child("allUserReviews")
-	var resortNameRef = reviewsRef.child("resortName");
+	var reviewsRef = ref.child("allUserReviews");
+	//IFFY
+	var resortNameRef = ref.child("reviews");
 	var resortDescRef = reviewsRef.child("resortDesc");
 
 	service.resortName = $firebaseArray(resortNameRef);
 	service.resortDesc = $firebaseArray(resortDescRef);
 
-	service.storeReview = function(storedResortName, storedResortDesc) {
+	service.storeReview = function(storedRating, storedResortName, storedResortDesc) {
 		service.resortName.$add({
+			rating: storedRating,
 			resortName: storedResortName,
 			resortDesc: storedResortDesc,
 			// userId: $scope.newUser.firstName,
 			time: Firebase.ServerValue.TIMESTAMP
 		}).then(function(){
-			$scope.skiResortName = '';
+			storedResortName = '';
 		})
 	}
 	return service;
 
 });
+
 
